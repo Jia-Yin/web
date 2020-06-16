ea360d850d42b83c4fee5e555f4865d2253dc9e1
Jia-Yin <jywglady@gmail.com>
2020-06-16 09:01:40 +0800

    Update

---
+++ examsite/_nuxt/1f55cdc3bfb7dc3b3877.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{839:function(n,t,e){"use strict";e.r(t);var l={middleware:["authadmin"],layout:"userlayout"},r=e(31),component=Object(r.a)(l,(function(){var n=this.$createElement,t=this._self._c||n;return t("b-container",[t("nuxt-child")],1)}),[],!1,null,null,null);t.default=component.exports}}]);
