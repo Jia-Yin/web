1e6f08a9712817b70990f0a32bf962c1c85d35f5
Jia-Yin <jywglady@gmail.com>
2020-06-23 08:24:23 +0800

    Update

--- examsite/_nuxt/36959bca764532c92ea0.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{920:function(e,n,t){"use strict";t.r(n);var r=t(33),component=Object(r.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("div",[this._v("Something wrong...Ask your teacher!")])}),[],!1,null,null,null);n.default=component.exports}}]);
