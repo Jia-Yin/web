8b089d193a3bb2f2fe0848d7e6dfc03d997b435f
Jia-Yin <jywglady@gmail.com>
2021-03-08 06:58:40 +0800

    Update

---
+++ codinghere/_nuxt/b241272.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{1067:function(e,n,t){"use strict";t.r(n);var r={created:function(){this.$router.push("/admin/courses")}},c=t(70),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);n.default=component.exports}}]);
