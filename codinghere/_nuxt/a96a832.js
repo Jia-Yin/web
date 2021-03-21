9d92dd65ce73a305e9432756d9417343d11cc6bc
Jia-Yin <jywglady@gmail.com>
2021-03-21 18:00:14 +0800

    Update CodingHere: Exam Fx

--- codinghere/_nuxt/a96a832.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[21],{1079:function(e,t,n){"use strict";n.r(t);var r={created:function(){this.$router.push("/user/course")}},c=n(70),component=Object(c.a)(r,(function(){var e=this.$createElement;return(this._self._c||e)("div")}),[],!1,null,null,null);t.default=component.exports}}]);
