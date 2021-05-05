a2d742a88ce311b0e4f3331e277617c0c776d07f
Jia-Yin <jywglady@gmail.com>
2021-05-05 23:53:09 +0800

    Update

--- codinghere/_nuxt/17da026.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[27],{1158:function(t,e,n){"use strict";n.r(e);var c={layout:"empty",created:function(){console.log("HERE"),this.$router.push("/contest/c2021")}},o=n(55),component=Object(o.a)(c,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("nuxt-child")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
