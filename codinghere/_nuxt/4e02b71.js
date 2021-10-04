a3019ffb079e51af9c2a676d061fd2d9035900d6
Jia-Yin <jywglady@gmail.com>
2021-10-04 20:39:46 +0800

    English Interface

--- codinghere/_nuxt/4e02b71.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{1377:function(t,e,n){"use strict";n.r(e);var c={layout:"empty",created:function(){this.$router.push("/contest/c2021")}},l=n(55),component=Object(l.a)(c,(function(){var t=this.$createElement,e=this._self._c||t;return e("div",[e("nuxt-child")],1)}),[],!1,null,null,null);e.default=component.exports}}]);
