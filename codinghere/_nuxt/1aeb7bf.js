3a526e271f5419c4aebd700e581d72fd3a85589e
Jia-Yin <jywglady@gmail.com>
2021-12-09 11:08:21 +0800

    Update Assistant

---
+++ codinghere/_nuxt/1aeb7bf.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{1422:function(t,e,n){"use strict";n.r(e);var r={created:function(){this.$router.push("/teacher/"+this.$route.params.cid+"/students")}},c=n(55),component=Object(c.a)(r,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("Teacher")])}),[],!1,null,null,null);e.default=component.exports}}]);
---
+++ codinghere/_nuxt/a3add9c.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[46],{1415:function(t,n,e){"use strict";e.r(n);var o={created:function(){this.$store.dispatch("logout")}},c=e(55),component=Object(c.a)(o,(function(){var t=this.$createElement;return(this._self._c||t)("div",[this._v("\n    Just For Exit...\n")])}),[],!1,null,null,null);n.default=component.exports}}]);
